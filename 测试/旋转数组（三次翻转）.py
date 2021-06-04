31715d260e86d6e37bf06e184e387491c83a3758
xuxin.ma <xuxin.ma@mihoyo.com>
2021-06-04 11:21:01 +0800

    添加算法笔记

---
+++ 测试/旋转数组（三次翻转）.py
@@ -0,0 +1,23 @@
+#给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+# 输入: nums = [1,2,3,4,5,6,7], k = 3
+# 输出: [5,6,7,1,2,3,4]
+# 解释:
+# 向右旋转 1 步: [7,1,2,3,4,5,6]
+# 向右旋转 2 步: [6,7,1,2,3,4,5]
+# 向右旋转 3 步: [5,6,7,1,2,3,4]
+
+class Solution:
+    def rotate(self, nums, k):
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    
+    
